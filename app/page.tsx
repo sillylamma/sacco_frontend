d235d352ee902177d8aa1a0fa2718fc22a870983
Billy <billyosoro1@gmial.com>
2026-07-27 23:59:13 +0300

    first sacco commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
 
